aaacfa67a918855fed060a627ae92b15e3c0ae45
matthewharrilal <matthewharrilal@gmail.com>
2017-12-06 12:13:22 -0800

    Working on the meeting times for these courses

---
+++ CoursePlanner/MeetingTimeTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MeetingTimeTableViewController.swift
+//  CoursePlanner
+//
+//  Created by Matthew Harrilal on 12/6/17.
+//  Copyright © 2017 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MeetingTimesTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+}
