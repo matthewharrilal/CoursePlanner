883a0aa91528a414ef95a109511dd23d9e4b68cd
matthewharrilal <matthewharrilal@gmail.com>
2017-12-05 17:27:27 -0800

    Working on adding the course

---
+++ CoursePlanner/AddCourseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddCourseViewController.swift
+//  CoursePlanner
+//
+//  Created by Matthew Harrilal on 12/5/17.
+//  Copyright © 2017 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddCourseTableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
