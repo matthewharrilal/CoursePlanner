52beee231fea02d951d78d080abfcb720390c46b
matthewharrilal <matthewharrilal@gmail.com>
2017-12-06 01:06:24 -0800

    Getting the values in core data

--- CoursePlanner/DisplayCoursesTableViewController.swift
+++ CoursePlanner/DisplayCoursesTableViewController.swift
@@ -18,7 +18,9 @@ class DisplayCourseViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         let fetchRequest = NSFetchRequest<Course>(entityName: "Course")
         do {
             let result = try? CoreDataStack.instance.viewContext.fetch(fetchRequest)
@@ -31,52 +33,26 @@ class DisplayCourseViewController: UITableViewController {
             print("Fatal error in the fetch request \(error), \(error?.localizedDescription)")
         }
         
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
         tableView.dataSource = dataSource
-        dataSource.items = courses
+        dataSource.items = self.courses
         
-        // MARK: TODO Abstract the fetch request to core data as well as saving values there
-//        let fetchRequest = NSFetchRequest<Course>(entityName: "Course")
-//        do {
-//            let result = try? CoreDataStack.instance.viewContext.fetch(fetchRequest)
-//            self.courses = result!
-//            print("This is the result from the fetch \(result)")
-//            self.tableView.reloadData()
-//        }
-//        catch {
-//            let error = error as NSError?
-//            print("Fatal error in the fetch request \(error), \(error?.localizedDescription)")
-//        }
         
         dataSource.configureCell = {(tableView, indexPath) -> UITableViewCell in
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
             let courseName = self.courses[indexPath.row]
             cell?.textLabel?.text = courseName.nameOfCourse
-            
+            //self.tableView.reloadData()
             return cell!
+            
         }
         
-        
-        
+        self.tableView.reloadData()
     }
+   
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func unwindToCourses(segue: UIStoryboardSegue) {
-        let fetchRequest = NSFetchRequest<Course>(entityName: "Course")
-        do {
-            let result = try? CoreDataStack.instance.viewContext.fetch(fetchRequest)
-            self.courses = result!
-            print("This is the result from the fetch \(result)")
-            self.tableView.reloadData()
-        }
-        catch {
-            let error = error as NSError?
-            print("Fatal error in the fetch request \(error), \(error?.localizedDescription)")
-        }
-    }
+   
 }
